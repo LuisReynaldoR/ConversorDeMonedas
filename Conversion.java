d12c5868e269fc9189b9605c43759ae5f40d6468
LuisReynaldoR <ramirezdeaquinol@gmail.com>
2025-05-08 12:30:38 -0600

    Create Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,10 @@
+import com.google.gson.annotations.SerializedName;
+
+public class Conversion {
+    @SerializedName("conversion_result")
+    private double resultado;
+
+    public double getResultado() {
+        return resultado;
+    }
+}
